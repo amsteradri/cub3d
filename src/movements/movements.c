a5a5da6fe6a5b9978f9723ed5514b230dd4ae0a2
amsteradri <amsteradri@gmail.com>
2023-12-07 13:06:05 +0100

    movimientos mas o menos clean y ya no se ve a traves de esquinas

--- src/movements/movements.c
+++ src/movements/movements.c
@@ -26,7 +26,7 @@ static void move_forward(t_map *map)
     double new_x = map->player->x + (cos(map->ray->angle) * 0.5);
     double new_y = map->player->y + (sin(map->ray->angle) * 0.5);
 
-    if (is_valid_move((int)new_y, (int)new_x, map))
+    if (is_valid_move((int)floor(new_y), (int)floor(new_x), map))
     {
         map->player->x = new_x;
         map->player->y = new_y;
@@ -38,7 +38,7 @@ static void move_backward(t_map *map)
     double new_x = map->player->x - (cos(map->ray->angle) * 0.5);
     double new_y = map->player->y - (sin(map->ray->angle) * 0.5);
 
-    if (is_valid_move((int)new_y, (int)new_x, map))
+    if (is_valid_move((int)floor(new_y), (int)floor(new_x), map))
     {
         map->player->x = new_x;
         map->player->y = new_y;
@@ -50,7 +50,7 @@ static void move_left(t_map *map)
     double new_x = map->player->x - (sin(map->ray->angle) * 0.5);
     double new_y = map->player->y + (cos(map->ray->angle) * 0.5);
 
-    if (is_valid_move((int)new_y, (int)new_x, map))
+    if (is_valid_move((int)floor(new_y), (int)floor(new_x), map))
     {
         map->player->x = new_x;
         map->player->y = new_y;
@@ -62,12 +62,13 @@ static void move_right(t_map *map)
     double new_x = map->player->x + (sin(map->ray->angle) * 0.5);
     double new_y = map->player->y - (cos(map->ray->angle) * 0.5);
 
-    if (is_valid_move(new_y, new_x, map))
+    if (is_valid_move((int)floor(new_y), (int)floor(new_x), map))
     {
         map->player->x = new_x;
         map->player->y = new_y;
     }
 }
+
 
 static void	move_camera_left(t_map *map)
 {
@@ -83,41 +84,41 @@ static void move_camera_right(t_map *map)
 		map->ray->angle -= (2 * M_PI);
 }
 
-int	move_character(int keycode, t_map *map)
-{
-	if (keycode == 53)
-		handle_esc_screen(map);
-	else if (keycode == 0)
-		move_right(map);
-	else if (keycode == 13)
-		move_forward(map);
-	else if (keycode == 2)
-		move_left(map);
-	else if (keycode == 1)
-		move_backward(map);
-	else  if (keycode == 123)
-		move_camera_left(map);
-	else if (keycode == 124)
-		move_camera_right(map);
-	return (0);
-}
-
-// int move_character(int keycode, t_map *map)
+// int	move_character(int keycode, t_map *map)
 // {
-//      if (keycode == KEY_ESC)
-//         handle_esc_screen(map);
-//     else if (keycode == KEY_A)
-//         move_left(map);
-//     else if (keycode == KEY_W)
-//         move_up(map);
-//     else if (keycode == KEY_D)
-//         move_right(map);
-//     else if (keycode == KEY_S)
-//         move_down(map);
-//     else if (keycode == KEY_LEFT_ARROW)
-//         move_camera_left(map);
-//     else if (keycode == KEY_RIGHT_ARROW)
-//         move_camera_right(map);
-
-//     return (0);
+// 	if (keycode == 53)
+// 		handle_esc_screen(map);
+// 	else if (keycode == 0)
+// 		move_right(map);
+// 	else if (keycode == 13)
+// 		move_forward(map);
+// 	else if (keycode == 2)
+// 		move_left(map);
+// 	else if (keycode == 1)
+// 		move_backward(map);
+// 	else  if (keycode == 123)
+// 		move_camera_left(map);
+// 	else if (keycode == 124)
+// 		move_camera_right(map);
+// 	return (0);
 // }
+
+int move_character(int keycode, t_map *map)
+{
+     if (keycode == KEY_ESC)
+        handle_esc_screen(map);
+    else if (keycode == KEY_A)
+        move_left(map);
+    else if (keycode == KEY_W)
+        move_forward(map);
+    else if (keycode == KEY_D)
+        move_right(map);
+    else if (keycode == KEY_S)
+        move_backward(map);
+    else if (keycode == KEY_LEFT_ARROW)
+        move_camera_left(map);
+    else if (keycode == KEY_RIGHT_ARROW)
+        move_camera_right(map);
+
+    return (0);
+}
